00b8242ecbf48bd1a077ccf6e20a86acf5307390
ardalanmemar93 <ardalanmemar@gmail.com>
2024-06-24 19:05:24 -0700

    testing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
+#... test ...
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
