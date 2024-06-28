217c923ca368654ff5dc1d756904747f37f13d84
Amit Deshpande <amitdeshpande@theinke.com>
2024-06-28 18:59:39 -0400

    --

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, ForgotPasswordRequestView
+from api.views import CreateUserView, ForgotPasswordRequestView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
