372f288bd700f5441bb3008f56ba313bf9db22d4
ardalanmemar93 <ardalanmemar@gmail.com>
2024-07-01 10:48:51 -0700

    Like model setup

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.contrib.auth.models import User
 
 class UserProfile(AbstractUser):
     """
