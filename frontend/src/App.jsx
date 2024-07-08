59f9e91c2e6c2ff51243640d3bdfed114fa5ee01
autture06 <arya.utture@gmail.com>
2024-07-08 15:25:02 -0400

    forgot password screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,36 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screens/LoginScreen';
-import AuthStatusScreen from './screens/AuthStatusScreen';
-import EditProfileScreen from './screens/EditProfileScreen';
-import ProtectedRoute from './components/ProtectedRoute';
+
+import LoginScreen from './components/LoginPage/LoginScreen';
+import ForgotPasswordScreen from './components/LoginPage/ForgotPasswordScreen';
+import AuthStatusScreen from './components/LoginPage/AuthStatusScreen';
+import EditProfileScreen from './components/LoginPage/EditProfileScreen';
+import ProtectedRoute from './components/LoginPage/ProtectedRoute';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const AuthStack = () => (
     <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }} // Hide the header for the login screen
+        />
+        <Stack.Screen
+            name="ForgotPassword"
+            component={ForgotPasswordScreen}
+            options={{ headerShown: false }} // Hide the header for the forgot password screen
+        />
     </Stack.Navigator>
 );
 
 const MainTab = () => (
     <Tab.Navigator>
-        <Tab.Screen name="AuthStatusScreen" component={AuthStatusScreen} />
+        <Tab.Screen name="AuthStatus" component={AuthStatusScreen} />
         <Tab.Screen
-            name="EditProfileScreen"
+            name="EditProfile"
             component={EditProfileScreenWrapper}
             options={{
                 tabBarLabel: 'Edit Profile',
