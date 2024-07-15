ba64b48a64dbab98d9f4a2a47189201d688a2088
Vishal Patel - work <vishpat@theinke.com>
2024-07-15 18:54:03 -0400

    isolated the journal features from the rest of the code for merging

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,33 +3,6 @@ from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from .models import UserProfile, JournalEntry
 from .serializers import UserProfileSerializer, JournalEntrySerializer
-
-class CreateUserView(generics.CreateAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.AllowAny]
-
-class UserProfileListView(generics.ListAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [] #[permissions.IsAuthenticated]
-
-class UserProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        obj = super().get_object()
-        if obj != self.request.user:
-            raise PermissionDenied('Please login to access your profile')
-        
-        return obj
-
-    def delete(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
     
 class JournalEntryListCreateView(generics.ListCreateAPIView):
     """
