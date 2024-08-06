feb72f5ac02a76c7df9653eb3844bf3e4dfd8378
autture06 <arya.utture@gmail.com>
2024-08-06 08:56:09 -0400

    Search screen updated

---
+++ frontend/src/screens/SearchScreen.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from "react";
+import {View, Text, TextInput, Button, Image, ScrollView, TouchableOpacity, StyleSheet} from 'react-native';
+import { fetchSearchData } from "../services/api";
+
+const SearchScreen = () => {
+    const [data, setData] = useState({
+        popular_searches: [],
+        articles: [],
+        videos: [],
+        podcasts: []
+    });
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const searchData = await fetchSearchData();
+                setData(searchData);
+            }
+            catch(error) {
+                console.error('Failed to fetch data', error)
+            }
+        };
+        getData();
+    }, []);
+
+    return (
+        <Text>Popular Searches</Text>
+    )
+}
