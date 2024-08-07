68e30cfb65b75eb8b985693341ac85d632afb371
autture06 <arya.utture@gmail.com>
2024-08-07 07:43:57 -0400

    SearchScreen added

---
+++ frontend/src/screens/SearchScreen.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+import { login } from '../services/api'; // Import login function from api.js
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ACCESS_TOKEN } from '../constants';
+
+const LoginScreen = ({ navigation }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const response = await login(username, password);
+            console.log('LOGIN SUCCESS | ln16');    //testing login
+
+            const accessToken = response.data.access;
+            // console.log("THIS IS ACCESS TOKEN", accessToken);  //printing accessToken from the data
+
+            await AsyncStorage.setItem(ACCESS_TOKEN, accessToken);
+            // const storedToken = await AsyncStorage.getItem(ACCESS_TOKEN);
+            // console.log('TOKEN STORED IN ASYNC STORAGE:', storedToken);
+
+            // Navigate to the main screen or perform other actions
+            navigation.navigate('Main');
+        } catch (error) {
+            console.error('Login failed:', error);
+        }
+    };
+
+    return (
+        <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <TextInput
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+            />
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+            <Button title="Login" onPress={handleLogin} />
+        </View>
+    );
+};
+
+export default LoginScreen;
