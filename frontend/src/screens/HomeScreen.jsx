d642c06dceb31bb4272eff6f5154a0165897a038
autture06 <arya.utture@gmail.com>
2024-08-08 05:03:30 -0400

    temporary home screen added for testing purposes

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,27 @@
+//Temporary Home Screen to implement the proper navigation from SearchScreen to HomeScreen
+
+// HomeScreen.jsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default HomeScreen;
