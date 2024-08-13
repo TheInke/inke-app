b35fb09a9c41e3ed4b3f2ec494883e490e6e3680
Amit Deshpande <amitdeshpande@theinke.com>
2024-08-13 14:30:27 -0400

    --

--- frontend/src/screens/CheckpointsScreen.jsx
+++ frontend/src/screens/CheckpointsScreen.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
-import Animated, {interpolate, useAnimatedStyle, useSharedValue} from 'react-native-reanimated'
+import Animated, {interpolate, useAnimatedStyle, useSharedValue, withDelay, withTiming} from 'react-native-reanimated'
 
 //import { login } from '../services/api'; // Import login function from api.js
 //import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -10,26 +10,40 @@ import Animated, {interpolate, useAnimatedStyle, useSharedValue} from 'react-nat
 const Ripple = ({ delay }) => 
 {
   const ripple = useSharedValue(0);
-  useAnimatedStyle( () => {
+  const style = useAnimatedStyle( () => {
     return {
       opacity: 0.8 - ripple.value,
       transform: [
         {
-          scale: interpolate(ring.value, )
+          scale: interpolate(ripple.value, [0,1], [0,4]),
         },
       ],
     };
   });
 
-  return <Animated.View style={styles.ring}/>
+  useEffect( () => {
+    ripple.value = withDelay(
+      delay,
+      withRepeat(
+        withTiming(1, {
+          duration: 4000,
+        }),
+        -1
+    )
+    );
+  }, []);
+  
+  return <Animated.View style={[styles.ripple, style]}/>
 }
     
 
 
-export default function CheckpointsScreen() 
-{
+export default CheckpointsScreen = () => {
   <View style={styles.container}>
-    <Ripple />
+    <Ripple delay={0}/>
+    <Ripple delay={1000}/>
+    <Ripple delay={2000}/>
+    <Ripple delay={3000}/>
   </View>
 };
 
@@ -43,7 +57,7 @@ const styles = StyleSheet.create
       justifyContent: 'center',
       backgroundColor: 'black'
     },
-    ring: 
+    ripple: 
     {
       position: absolute,
       width: 100,
