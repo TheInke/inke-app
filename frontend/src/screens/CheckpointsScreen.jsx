ace22e44780c2b8cc77acaff9d45d0ae3ee7bdb1
Amit Deshpande <amitdeshpande@theinke.com>
2024-08-13 09:53:10 -0400

    - taking new approach to animation - installed react-native-reanimated dependency - run 'npm install' prior to simluator

--- frontend/src/screens/CheckpointsScreen.jsx
+++ frontend/src/screens/CheckpointsScreen.jsx
@@ -1,76 +1,56 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
+import Animated, {interpolate, useAnimatedStyle, useSharedValue} from 'react-native-reanimated'
 
 //import { login } from '../services/api'; // Import login function from api.js
 //import AsyncStorage from '@react-native-async-storage/async-storage';
 //import { ACCESS_TOKEN } from '../constants';
 
 
-const CheckpointsScreen = () => {
+const Ripple = ({ delay }) => 
+{
+  const ripple = useSharedValue(0);
+  useAnimatedStyle( () => {
+    return {
+      opacity: 0.8 - ripple.value,
+      transform: [
+        {
+          scale: interpolate(ring.value, )
+        },
+      ],
+    };
+  });
 
-  const rippleSize = useRef(new Animated.Value(100)).current;
-  const rippleOpacity = useRef(new Animated.Value(1)).current;
+  return <Animated.View style={styles.ring}/>
+}
+    
 
-  const handlePress = () => {
-    rippleSize.setValue(100);
-    rippleOpacity.setValue(1);
-    Animated.parallel([
-      Animated.timing(rippleSize, {
-        toValue: 200, // Scale up to full size
-        duration: 4000, // Duration of the animation
-        useNativeDriver: false,
-      }),
-      Animated.timing(rippleOpacity, {
-        toValue: 0,
-        duration: 500, // Duration of the fade-out effect
-        useNativeDriver: true,
-      }),
-    ]).start();
-  };
 
-  const styles = StyleSheet.create({
-    container: {
+export default function CheckpointsScreen() 
+{
+  <View style={styles.container}>
+    <Ripple />
+  </View>
+};
+
+const styles = StyleSheet.create
+(
+  {
+    container: 
+    {
       flex: 1,
-      backgroundColor: 'black',
-      justifyContent: 'center',
       alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'black'
     },
-    circle: {
+    ring: 
+    {
+      position: absolute,
       width: 100,
       height: 100,
       borderRadius: 50,
-      backgroundColor: 'rgba(128, 128, 128, 0.5)',
-      justifyContent: 'center',
-      alignItems: 'center',
-      overflow: 'hidden',
-    },
-    ripple: {
-      position: 'absolute',
-      width: rippleSize,  // Initial size of the ripple
-      height: rippleSize,
-      opacity: rippleOpacity,
-      borderRadius: 50,
-      backgroundColor: 'rgba(211, 211, 211, 0.5)', // Light gray color for the ripple effect
-    },
-  });
+      backgroundColor: 'gray',
+    }
+  }
+)
 
-
-  return (
-    <View style={styles.container}>
-
-      <View style={styles.circle}>
-        <Text>hi</Text>
-      </View>
-
-      <TouchableWithoutFeedback onPress={handlePress}>
-        <Animated.View
-          style={styles.ripple}
-        />
-      </TouchableWithoutFeedback>
-    </View>
-  );
-};
-
-
-
-export default CheckpointsScreen;
