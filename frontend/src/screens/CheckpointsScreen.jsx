a705ba846fdab1468723c38db9a4b49aeb50f5a3
Amit Deshpande <amitdeshpande@theinke.com>
2024-08-15 11:44:34 -0400

    --

--- frontend/src/screens/CheckpointsScreen.jsx
+++ frontend/src/screens/CheckpointsScreen.jsx
@@ -1,31 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Animated, TouchableOpacity, Dimensions } from 'react-native';
+import React, { useEffect, useState, useRef} from 'react';
+import { View, Text, StyleSheet, Animated, TouchableOpacity, Dimensions, FlatList} from 'react-native';
 
 export default CheckpointsScreen = () => {
+  /*
   const size = new Animated.Value(1);
   const instructionFontSize = new Animated.Value(40);
   const opacity = new Animated.Value(0);
+  */
+
+  const size = useRef(new Animated.Value(1)).current;
+  const opacity = useRef(new Animated.Value(1)).current;
+  const animatedFontSize = useRef(new Animated.Value(40)).current;
+
+  const instructionText = [
+    {instruction:'Drag the timer to choose duration.\nRelease to start.', fontSize: 20},
+    {instruction:'Inhale', fontSize: animatedFontSize},
+    {instruction:'Hold', fontSize: animatedFontSize},
+    {instruction:'Exhale', fontSize: animatedFontSize},
+  ]
+
+  const [instructionTextIndex, setInstructionTextIndex] = useState(0);
 
   const [animationRunning, setAnimationRunning] = useState(false);  // Track animation state
 
   // Function to start the animation
   const startAnimation = () => {
-    const instructionFontAnimation = Animated.sequence([
-      Animated.timing(instructionFontSize, {
+    
+    const inhaleAnimation = () => 
+    {
+      setInstructionTextIndex(1);
+      Animated.timing(animatedFontSize, {
         toValue: 60,
         duration: 4000,
         useNativeDriver: false,
-      }),
-      Animated.timing(instructionFontSize, {
+      })
+    }
+    const holdAnimation = () => 
+    {
+      setInstructionTextIndex(2);
+      Animated.timing(animatedFontSize, {
         toValue: 60,
         duration: 1000,
         useNativeDriver: false,
-      }),
-      Animated.timing(instructionFontSize, {
+      })
+    }
+    const exhaleAnimation = () => 
+    {
+      setInstructionTextIndex(3);
+      Animated.timing(animatedFontSize, {
         toValue: 40,
         duration: 8000,
         useNativeDriver: false,
-      }),
+      })
+    }
+
+    const instructionFontAnimation = Animated.sequence([
+      inhaleAnimation,
+      holdAnimation,
+      exhaleAnimation,
     ]);
 
     const sizeAnimation = Animated.sequence([
@@ -66,17 +98,23 @@ export default CheckpointsScreen = () => {
 
   // Stop the animation by resetting the values
   const stopAnimation = () => {
+    
     size.stopAnimation();
+    size.setValue(1);
     opacity.stopAnimation();
-    instructionFontSize.stopAnimation();
+    opacity.setValue(1);
+    animatedFontSize.stopAnimation();
+    animatedFontSize.setValue(40);
   };
 
   // Handle the tap event
   const handleTap = () => {
     console.log(animationRunning);
     if (animationRunning) {
+      console.log('trying stopAnimation');
       stopAnimation();  // Stop animation if running
     } else {
+      console.log('trying startAnimation');
       startAnimation();  // Start animation if stopped
     }
     setAnimationRunning(!animationRunning);  // Toggle animation state
@@ -107,6 +145,7 @@ export default CheckpointsScreen = () => {
       justifyContent: 'center',
       padding: 10,
       marginTop: (154.6 / 2) - 30,
+      zIndex: 1
 
       //borderColor: 'blue',
       //borderWidth: 5,
@@ -116,7 +155,7 @@ export default CheckpointsScreen = () => {
       height: 170,
       width: 170,
       borderRadius: 85,
-      zIndex: 1,  // Ensure touch is captured
+      zIndex: 2,  // Ensure touch is captured
     },
     ripple: {
       position: 'absolute',
@@ -127,21 +166,22 @@ export default CheckpointsScreen = () => {
       opacity: opacity,
       transform: [{ scale: size }],
       zIndex: -1,  // Make sure it's behind the timer for tap functionality
+      elevation: -1
     },
     touchable:
     {
-      borderColor: 'red',
-      borderWidth: 5,
+      zIndex: 3
     },
   });
 
   return (
     <View style={styles.screenContainer}>
       <View style={styles.topHalf}>
-        <Animated.Text style={[styles.header, { fontSize: instructionFontSize }]}>
-          Inhale
+        <Animated.Text style={[styles.header, { fontSize: instructionText[instructionTextIndex].fontSize }]}>
+          {instructionText[instructionTextIndex].instruction}
         </Animated.Text>
       </View>
+
       {/*
       <TouchableOpacity onPress={handleTap} activeOpacity={0.9}>
         <View style={styles.timerContainer}>
@@ -149,9 +189,14 @@ export default CheckpointsScreen = () => {
           <Animated.View style={styles.ripple} />
         </View>
       </TouchableOpacity>
+
+      <TouchableOpacity style={styles.touchable} onPress={ () => handleTap()}>
+            <View style={styles.timer}></View>
+          </TouchableOpacity>
        TouchableOpacity for tap detection */}
+
         <View style={styles.timerContainer}>
-          <TouchableOpacity style={styles.touchable} onPress={handleTap}>
+          <TouchableOpacity style={styles.touchable} activeOpacity={0.8} onPress={handleTap}>
             <View style={styles.timer}></View>
           </TouchableOpacity>
           <Animated.View style={styles.ripple} />
