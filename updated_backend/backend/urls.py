5b759bc5c64a845da3ba28efb2d3dc68108111f4
Amit Deshpande <amitdeshpande@theinke.com>
2024-08-23 10:46:40 -0400

    --

--- updated_backend/backend/urls.py
+++ updated_backend/backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import CustomTokenObtainPairView
 
 # post 
 from django.conf import settings
@@ -8,7 +9,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
