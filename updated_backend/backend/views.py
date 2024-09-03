e346dd3575e804babbe7c3b000eba13ef6eb8e80
Amit Deshpande <amitdeshpande@theinke.com>
2024-09-03 14:51:37 -0400

    - deleted old backend folders - updated_backend_2 is the latest backend

--- updated_backend/backend/views.py
+++
@@ -1,6 +0,0 @@
-# views.py
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import CustomTokenObtainPairSerializer
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
